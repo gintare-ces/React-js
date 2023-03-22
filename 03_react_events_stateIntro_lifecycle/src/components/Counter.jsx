1e80a284a3bdee6375f8b36942525d8ad09fe131
gintare-ces <gintare.cesiuliene@gmail.com>
2023-03-22 11:16:25 +0200

    button counter

--- 03_react_events_stateIntro_lifecycle/src/components/Counter.jsx
+++ 03_react_events_stateIntro_lifecycle/src/components/Counter.jsx
@@ -1,15 +1,33 @@
+import {useState} from 'react';
 import UiCard from "./ui/UiCard";
 
 const Counter = () => {
     console.log('Counter mounted');
     // logic
     // paprastas kintamasis neveikia jei reikia kintamos reiksmes JSX
-    let count = 0;
+  
+    // const [kintamojo pavadinimas, funkcija nustatyti nauja reiksme] = useState(pradine kintamojo reiksme)
+    const [count, setCount] = useState(0)
 
     const handleIncrement = () => {
-        console.log('handleIncrement');
-        count = count + 1;
-        console.log('count ===', count);
+        console.log('handleIncrement')
+        // kai naujas state priklauso nuo pries tai buvusio turetume atnaujinti su funkcija
+        setCount(count + 1);
+    }
+
+    const handleDecrement = () => {
+        // mazetu zemyn
+        setCount(count - 1);
+    }
+    const handleChange = (howMuch) => {
+        // du vienoje funkcijoje
+        setCount(count + howMuch)
+
+    }
+    const handleReset = () => {
+        // reset
+        setCount(0)
+
     }
 
     return (
@@ -17,6 +35,10 @@ const Counter = () => {
         <h3>Counter</h3>
         <h2>{count}</h2>
         <button onClick={handleIncrement}>Plus</button>
+        <button onClick={handleDecrement}>Minus</button>
+        <button onClick={() => handleChange(5)}>Add 5</button>
+        <button onClick={() => handleChange(10)}>Add 10</button>
+        <button onClick={handleReset}>Reset Counter</button>
      </UiCard>
     )
 }
