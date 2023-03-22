0bb216d207b9f914bbe9f8d418708300f6ddd840
gintare-ces <gintare.cesiuliene@gmail.com>
2023-03-22 10:34:48 +0200

    simple variable does not work

--- 03_react_events_stateIntro_lifecycle/src/components/Counter.jsx
+++ 03_react_events_stateIntro_lifecycle/src/components/Counter.jsx
@@ -3,13 +3,20 @@ import UiCard from "./ui/UiCard";
 const Counter = () => {
     console.log('Counter mounted');
     // logic
+    // paprastas kintamasis neveikia jei reikia kintamos reiksmes JSX
     let count = 0;
+
+    const handleIncrement = () => {
+        console.log('handleIncrement');
+        count = count + 1;
+        console.log('count ===', count);
+    }
 
     return (
      <UiCard>
         <h3>Counter</h3>
         <h2>{count}</h2>
-        <button>Plus</button>
+        <button onClick={handleIncrement}>Plus</button>
      </UiCard>
     )
 }
